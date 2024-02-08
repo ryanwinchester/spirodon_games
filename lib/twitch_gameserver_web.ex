e6ed405b98f1e61330a9b0798c58c9984be0d308
Ryan Winchester <ryan@winchester.dev>
2024-02-08 07:48:26 -0400

    favicons

--- lib/twitch_gameserver_web.ex
+++ lib/twitch_gameserver_web.ex
@@ -17,7 +17,7 @@ defmodule TwitchGameServerWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts icons images favicon.ico robots.txt site.webmanifest)
 
   def router do
     quote do
