0fcb30e766301fd66efcc1dfdb6996858b542743
Ryan Winchester <ryan@winchester.dev>
2024-02-25 02:53:38 -0400

    Update route and controller

--- lib/twitch_gameserver_web/controllers/game_controller.ex
+++ lib/twitch_gameserver_web/controllers/game_controller.ex
@@ -1,9 +1,10 @@
 defmodule TwitchGameServerWeb.GameController do
   use TwitchGameServerWeb, :controller
+
   plug :put_root_layout, false
   plug :put_layout, false
 
-  def index(conn, _params) do
-    render(conn, :index)
+  def show(conn, _params) do
+    render(conn, :show)
   end
 end
